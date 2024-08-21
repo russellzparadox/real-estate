19dbfb52d2adab040f9f695bc9efe57a6693c8cf
russellzparadox <russellzparadox@gmail.com>
2024-08-21 12:52:07 +0330

    rename page-agents-list-view-1.html to agents-list.html

--- agents/views.py
+++ agents/views.py
@@ -4,4 +4,4 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 class AgentListView(TemplateView):
-    template_name = 'page-agents-list-view-1.html'
+    template_name = 'agents-list.html'
