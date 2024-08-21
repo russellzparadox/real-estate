036516b764c6e4315fc36048f0b37174f3271f54
russellzparadox <russellzparadox@gmail.com>
2024-08-21 11:45:08 +0330

    fixed agents app

--- agents/views.py
+++ agents/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, TemplateView
+from django.views.generic import TemplateView
 
 
 # Create your views here.
 class AgentListView(TemplateView):
-    template_name = 'page-list-view-1.html'
+    template_name = 'page-agents-list-view-1.html'
